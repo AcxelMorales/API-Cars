4cca93343b0fd63115e1fc1e308f0d253a879f12
Acxel Morales <acxelmorales97@gmail.com>
2022-11-08 18:45:09 -0600

    Se crearon servicios de brands

---
+++ src/brands/brands.service.ts
@@ -0,0 +1,65 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { v4 as uuid } from 'uuid';
+
+import { CreateBrandDto } from './dto/create-brand.dto';
+import { UpdateBrandDto } from './dto/update-brand.dto';
+
+import { Brand } from './entities/brand.entity';
+
+@Injectable()
+export class BrandsService {
+
+  private brands: Brand[] = [
+    {
+      id: uuid(),
+      name: 'Ferrari',
+      createdAt: new Date().getTime(),
+    }
+  ];
+
+  create({ name }: CreateBrandDto): Brand {
+    const brand: Brand = {
+      id: uuid(),
+      name: name.toLocaleLowerCase(),
+      createdAt: new Date().getTime(),
+    };
+
+    this.brands.push(brand);
+
+    return brand;
+  }
+
+  findAll(): Brand[] {
+    return this.brands;
+  }
+
+  findOne(id: string): Brand {
+    const brand = this.brands.find(brand => brand.id == id);
+    if (!brand) throw new NotFoundException(`Brand with ID "${id}" not found`);
+    return brand;
+  }
+
+  update(id: string, updateBrandDto: UpdateBrandDto): Brand {
+    let brandDB = this.findOne(id);
+
+    this.brands = this.brands.map(b => {
+      if (b.id === id) {
+        brandDB.createdAt = new Date().getTime();
+        brandDB = {
+          ...brandDB,
+          ...updateBrandDto
+        };
+
+        return brandDB;
+      }
+    });
+
+    return brandDB;
+  }
+
+  remove(id: string): void {
+    this.brands = this.brands.filter(b => b.id !== id);
+  }
+
+}
